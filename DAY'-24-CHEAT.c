c6b73e734fd4f99593b6352c98b9f54440ae863b
Sourav <83556267+souravbot@users.noreply.github.com>
2024-10-26 10:56:11 +0530

    Add files via upload
    DAY 24 OF  CODING

---
+++ DAY'-24-CHEAT.c
@@ -0,0 +1,22 @@
+//day-24-of coding; dracula eats
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T); 
+
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+
+
+        int tuesdays = N / 7;           
+        if (N % 7 >= 2) tuesdays += 1;
+
+        printf("%d\n", tuesdays);
+    }
+
+    return 0;
+}
+
+
