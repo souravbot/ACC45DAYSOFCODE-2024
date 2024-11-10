2662d93dd111f0b0c1362347399a425b227254ca
Sourav <83556267+souravbot@users.noreply.github.com>
2024-11-11 03:02:41 +0530

    Add files via upload

---
+++ DAY-39-CYCLICQD.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);  //no of test cases
+
+    while (T--) {
+        int A, B, C, D;
+        scanf("%d %d %d %d", &A, &B, &C, &D); 
+        if ((A + C == 180) && (B + D == 180)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
