c319837c3439b1abe98ac618ba20d6ea76617887
Sourav <83556267+souravbot@users.noreply.github.com>
2024-11-12 00:38:35 +0530

    Add files via upload

---
+++ DAY-40-MOVIE2X.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+
+ 
+    int time_for_first_part = Y / 2;
+    
+    int time_for_remaining_part = X - Y;
+
+   
+    int total_time = time_for_first_part + time_for_remaining_part;
+
+    printf("%d\n", total_time);
+
+    return 0;
+}
+
+
